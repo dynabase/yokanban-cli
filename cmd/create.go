2f6429ec44427e574ef60cd905433fcb231898ce
Daniel Angileri <daniele@dynabase.de>
2021-03-26 08:46:15 +0100

    refac: make use subcommands to create yokanban resources

--- cmd/create.go
+++ cmd/create.go
@@ -8,18 +8,27 @@ import (
 
 var name string
 
-// createCmd represents the create command
+// createCmd represents the root create command
 var createCmd = &cobra.Command{
-	Use:       "create",
+	Use:       "create [element]",
 	Short:     "Create yokanban resources like boards, cards, etc.",
-	Long:      `Create yokanban resources like boards, cards, etc.`,
 	Example:   "yokanban create board --name test-board",
 	ValidArgs: []string{string(elements.Board)},
 	Args:      cobra.ExactValidArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if args[0] == string(elements.Board) {
-			api.CreateBoard(api.CreateBoardModel{Name: name})
-		}
+		//if args[0] == string(elements.Board) {
+		//	api.CreateBoard(api.CreateBoardModel{Name: name})
+		//}
+	},
+}
+
+// createBoardSubCmd has the responsibility to create a yokanban board
+var createBoardSubCmd = &cobra.Command{
+	Use:     "board",
+	Short:   "Create a yokanban board",
+	Example: "yokanban create board --name test-board",
+	Run: func(cmd *cobra.Command, args []string) {
+		api.CreateBoard(api.CreateBoardModel{Name: name})
 	},
 }
 
@@ -36,5 +45,8 @@ func init() {
 	// is called directly, e.g.:
 	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-	createCmd.Flags().StringVarP(&name, "name", "n", "", "The name of the resource")
+	createCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "The name of the resource")
+
+	// subCommands
+	createCmd.AddCommand(createBoardSubCmd)
 }
