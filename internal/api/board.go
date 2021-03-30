844ceaad7d463ff62e3518a1e5f337859b9d78f4
daniele <53174657+daniele-dynabase@users.noreply.github.com>
2021-03-30 10:50:52 +0200

    feat: reduce "list board" output to "_id", "name" and "createdAt" (#13)
    * feat: reduce "list board" output to "_id", "name" and "createdAt"
    
    * refac: remove DTO suffix from BoardShort and BoardList since those structs do not represent any no exchange format

--- internal/api/board.go
+++ internal/api/board.go
@@ -9,3 +9,20 @@ type BoardDTO struct {
 	CreatedAt string      `json:"createdAt"`
 	Avatars   []AvatarDTO `json:"avatars"`
 }
+
+// BoardShort represents the short version of a single yokanban board containing only the minimal dataset for an overview.
+type BoardShort struct {
+	ID        string `json:"_id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"createdAt"`
+}
+
+// BoardList represents a list of yokanban boards.
+type BoardList []*BoardShort
+
+// Map maps a BoardDTO to a BoardShort.
+func (b *BoardShort) Map(board *BoardDTO) {
+	b.ID = board.ID
+	b.Name = board.Name
+	b.CreatedAt = board.CreatedAt
+}
